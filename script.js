72946fc2a41a40a828868f2c863b3d42681b0026
matthalm <33130412+matthalm@users.noreply.github.com>
2023-04-29 14:52:34 -0400

    critters

---
+++ script.js
@@ -0,0 +1,4 @@
+function randomizecritters(); {
+	document.getelementbyID("rabbit").style.left = Math.random() + "vw";
+	document.getelementbyID("rabbit").style.margin-top = Math.random() * 100 + "px";
+}
