8a9571a7f73890e4d9c49cfc3148bfeaf99685d2
matthalm <33130412+matthalm@users.noreply.github.com>
2023-04-29 15:41:31 -0400

    reformatted for direct links
    home page links directly to external websites (no subpages)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function randomizecritters(); {
-	document.getelementbyID("rabbit").style.left = Math.random() + "vw";
-	document.getelementbyID("rabbit").style.margin-top = Math.random() * 100 + "px";
+	document.getelementbyID("rabbit1").style.left = Math.random() * 100 + "vw";
+	document.getelementbyID("rabbit1").style.margin-top = Math.random() * 100 + 100 + "px";
 }
