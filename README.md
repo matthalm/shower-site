7911b02865c4c3dd4e3f44a5e78572cbdd4e5468
matthalm <33130412+matthalm@users.noreply.github.com>
2023-04-30 11:52:56 -0400

    randomized critter placement

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shower-site
 virtual baby shower
+
+randomized critter placement via https://css-tricks.com/random-numbers-css/
--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-function randomizecritters(); {
-	document.getelementbyID("rabbit1").style.left = Math.random() * 100 + "vw";
-	document.getelementbyID("rabbit1").style.margin-top = Math.random() * 100 + 100 + "px";
-}
+var rabbit1 = document.querySelector('#rabbit1');
+rabbit1.style.setProperty('--rabbit1y', Math.random() * 95 + 'vw');
+rabbit1.style.setProperty('--rabbit1x', Math.random() * 200 + 100 + 'px');
+
+var rabbit2 = document.querySelector('#rabbit2');
+rabbit2.style.setProperty('--rabbit2y', Math.random() * 95 + 'vw');
+rabbit2.style.setProperty('--rabbit2x', Math.random() * 200 + 100 + 'px');
+
+var squirrel1 = document.querySelector('#squirrel1');
+squirrel1.style.setProperty('--squirrel1y', Math.random() * 95 + 'vw');
+squirrel1.style.setProperty('--squirrel1x', Math.random() * 200 + 'px');
+
+var squirrel2 = document.querySelector('#squirrel2');
+squirrel2.style.setProperty('--squirrel2y', Math.random() * 95 + 'vw');
+squirrel2.style.setProperty('--squirrel2x', Math.random() * 200 + 'px');
+
+var squirrel3 = document.querySelector('#squirrel3');
+squirrel3.style.setProperty('--squirrel3y', Math.random() * 95 + 'vw');
+squirrel3.style.setProperty('--squirrel3x', Math.random() * 200 + 'px');
+
+var daisy1 = document.querySelector('#daisy1');
+daisy1.style.setProperty('--daisy1y', Math.random() * 95 + 'vw');
+daisy1.style.setProperty('--daisy1x', Math.random() * 200 + 200 + 'px');
+
+var daisy2 = document.querySelector('#daisy2');
+daisy2.style.setProperty('--daisy2y', Math.random() * 95 + 'vw');
+daisy2.style.setProperty('--daisy2x', Math.random() * 200 + 200 + 'px');
+
+var daisy3 = document.querySelector('#daisy3');
+daisy3.style.setProperty('--daisy3y', Math.random() * 95 + 'vw');
+daisy3.style.setProperty('--daisy3x', Math.random() * 200 + 200 + 'px');
+
+var mushroom1 = document.querySelector('#mushroom1');
+mushroom1.style.setProperty('--mushroom1y', Math.random() * 95 + 'vw');
+mushroom1.style.setProperty('--mushroom1x', Math.random() * 200 + 100 + 'px');
